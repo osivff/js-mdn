98b670282519c38f9ac65a0269985dc28a623009
osivff <oscar.fitchfierro@gmail.com>
2025-03-13 05:25:47 -0700

    solution to ex-4

---
+++ 6.strings/ex-4/script.js
@@ -0,0 +1,7 @@
+const theorem = 'Pythagorean theorem';
+
+const a = 5;
+const b = 8;
+
+const myString = 
+`Using ${theorem}, we can work out that if the two shortest sides of a right-angled triangle have lengths of ${a} and ${b}, the length of the hypotenuse is ${Math.floor(((a*a) + (b*b))** 0.5)}.`;
