62a0ebf588b3138caf2f71d6627fc805bb103a3c
osivff <oscar.fitchfierro@gmail.com>
2025-03-21 05:11:52 -0700

    Add string and arrays and event listener to button

--- 8.challenge-silly-story-generator/script.js
+++ 8.challenge-silly-story-generator/script.js
@@ -2,7 +2,26 @@ const customName = document.getElementById("customname");
 const randomizeButton = document.querySelector(".randomize");
 const usTemp = document.getElementById("us");
 const ukTemp = document.getElementById("uk");
-let storyText = document.querySelector(".story");
+let story = document.querySelector(".story");
+// Text variables
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+let insertX = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"
+];
+let insertY = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House"
+];
+let insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"
+];
+
+
 
 function randomElementInArray(array){
     let randomNumber = Number(Math.floor(Math.random() * array.length));
@@ -18,3 +37,11 @@ function toFahrenheit(num){
     let formula = (num * 9/5) + 32;
     return Number(formula.toFixed(2));
 }
+
+function result(){
+    
+}
+
+// Text
+
+randomizeButton.addEventListener("click", result());
