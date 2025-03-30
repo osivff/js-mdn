12571d24fe647927b8d659de1a87b49bd07addc1
osivff <oscar.fitchfierro@gmail.com>
2025-03-30 09:58:03 -0700

    Create array of imagesclear

--- 14.img-gallery/main.js
+++ 14.img-gallery/main.js
@@ -3,16 +3,22 @@ const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
+const imgArr = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imgTxt = {
+    one: 'Weird and old looking thing',
+    two: 'Purple and white flowers',
+    three: 'Ancient egiptian drawings',
+    four: 'Big butterfly sitting on a leaf'
+};
 
-/* Declaring the array of image filenames */
+for(let img of imgArr){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${img}`);
+    thumbBar.appendChild(newImage);
+}
 
-/* Declaring the alternative text for each image file */
 
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+// newImage.setAttribute('alt', xxx);
+// thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
