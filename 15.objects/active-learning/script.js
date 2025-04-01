375ed2111ebf86a0598da811eda43733535929f4
osivff <oscar.fitchfierro@gmail.com>
2025-04-01 05:35:11 -0700

    Create constructor and method

--- 15.objects/active-learning/script.js
+++ 15.objects/active-learning/script.js
@@ -1,3 +1,5 @@
+//Creating Objects
+
 const person = {
     name: {
         first: 'Bob',
@@ -23,3 +25,29 @@ person.farewell = function(){
 const myDataName = "height";
 const myDataValue = "1.75m";
 person[myDataName] = myDataValue;
+
+// Introduction to constructors
+
+//Creating a function
+function createPerson(personName) {
+    const obj = {};
+    obj.personName = personName;
+    obj.introduceSelf = function () {
+        console.log(`Hi my name is ${this.personName}`);
+    }
+    return obj;
+}
+
+//Using a constructor pre-ES6 (plain javascript)
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.introduceSelf = function (){
+        console.log(`Hello my name is ${this.name} and I am ${this.age} years old`);
+    }
+}
+
+//Keyword --> "new" creates an object
+
+const silvia = new Person("Silvia", 89);
+const leonard = new Person("Leonard", 59);
