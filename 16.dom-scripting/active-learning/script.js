9195edd3d4276f6efc5f2e8e193c7f9664f5d160
osivff <oscar.fitchfierro@gmail.com>
2025-04-04 04:34:40 -0700

    Create an element in the DOM

--- 16.dom-scripting/active-learning/script.js
+++ 16.dom-scripting/active-learning/script.js
@@ -1,3 +1,7 @@
 const link = document.querySelector('a');
+const sect = document.querySelector('section');
+const para = document.createElement('p'); 
 link.textContent = "Mozilla Developers Network";
 link.href = "https://developer.mozilla.org";
+para.textContent = "We hoped you enjoyed the ride.";
+sect.appendChild(para);
