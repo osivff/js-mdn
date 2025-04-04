75581c9ef92f85900e31e446deef46b837cf986e
osivff <oscar.fitchfierro@gmail.com>
2025-04-04 04:44:53 -0700

    Create text node

--- 16.dom-scripting/active-learning/script.js
+++ 16.dom-scripting/active-learning/script.js
@@ -1,7 +1,13 @@
 const link = document.querySelector('a');
 const sect = document.querySelector('section');
-const para = document.createElement('p'); 
+const para = document.createElement('p');
+const text = document.createTextNode(
+    " - the premier source for web development knowledge"
+);
+const linkPara = document.querySelector('p');
+
 link.textContent = "Mozilla Developers Network";
 link.href = "https://developer.mozilla.org";
 para.textContent = "We hoped you enjoyed the ride.";
 sect.appendChild(para);
+linkPara.appendChild(text);
