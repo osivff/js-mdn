d6c44b31ee091a078bbd22c3c75f501bd733ec1f
osivff <oscar.fitchfierro@gmail.com>
2025-04-05 05:32:00 -0700

    Set class attribute to change para

--- 16.dom-scripting/active-learning/script.js
+++ 16.dom-scripting/active-learning/script.js
@@ -4,12 +4,15 @@ const para = document.createElement('p');
 const text = document.createTextNode(
     " - the premier source for web development knowledge"
 );
+const paraTwo = document.createElement('p');
 const linkPara = document.querySelector('p');
 
 link.textContent = "Mozilla Developers Network";
 link.href = "https://developer.mozilla.org";
 para.textContent = "We hoped you enjoyed the ride.";
+paraTwo.textContent = "This change was made setting the class attribute in javascript";
 sect.appendChild(para);
+sect.appendChild(paraTwo);
 linkPara.appendChild(text);
 // sect.removeChild(linkPara);
 
@@ -19,3 +22,5 @@ para.style.backgroundColor = 'black';
 para.style.padding = '2em';
 para.style.width = '250px';
 para.textAlign = 'center';
+
+paraTwo.setAttribute("class", "highlight");
