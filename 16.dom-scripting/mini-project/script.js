9dffea50550bf868fee500a1a5e5d325952c32da
osivff <oscar.fitchfierro@gmail.com>
2025-04-06 04:34:28 -0700

    Create delete button next to item

--- 16.dom-scripting/mini-project/script.js
+++ 16.dom-scripting/mini-project/script.js
@@ -2,16 +2,23 @@ const item = document.querySelector('#item');
 const preview = document.querySelector('.preview');
 const submit = document.querySelector('.submit');
 const list = document.createElement('ul');
+const deleteButton = document.createElement('button');
+const itemValue = item.value;
+const displayItem = document.createElement('li');
+
 
 function addItem(){
-    const itemValue = item.value;
-    const displayItem = document.createElement('li');
-    
     displayItem.textContent = itemValue;
+    displayItem.style.margin = "1rem";
+    deleteButton.textContent = "Delete";
+    deleteButton.style.marginLeft = "1rem";
+
+    displayItem.appendChild(deleteButton);
     list.appendChild(displayItem);
     preview.appendChild(list);
-}
 
+
+}
 
 submit.addEventListener("click", addItem);
 
