db657dcf9803d31429baa738c515d1e917d271d5
osivff <oscar.fitchfierro@gmail.com>
2025-04-06 04:58:14 -0700

    Delete item from list

--- 16.dom-scripting/mini-project/script.js
+++ 16.dom-scripting/mini-project/script.js
@@ -2,23 +2,26 @@ const item = document.querySelector('#item');
 const preview = document.querySelector('.preview');
 const submit = document.querySelector('.submit');
 const list = document.createElement('ul');
-const deleteButton = document.createElement('button');
-const itemValue = item.value;
-const displayItem = document.createElement('li');
 
 
 function addItem(){
+    const deleteButton = document.createElement('button');
+    const itemValue = item.value;
+    const displayItem = document.createElement('li');
+    
+    deleteButton.setAttribute('class', 'delete');
     displayItem.textContent = itemValue;
-    displayItem.style.margin = "1rem";
     deleteButton.textContent = "Delete";
-    deleteButton.style.marginLeft = "1rem";
 
     displayItem.appendChild(deleteButton);
     list.appendChild(displayItem);
     preview.appendChild(list);
 
+    deleteButton.addEventListener("click", () => {
+        list.removeChild(displayItem);
+    });
 
 }
 
-submit.addEventListener("click", addItem);
 
+submit.addEventListener("click", addItem);
